45a435beb808aa6bb6d3b56a8561bcd8760b9225
kishikishore2863 <kishikishore.2863@gmail.com>
2025-06-27 23:28:43 +0530

    otp handler

--- src/main/java/com/sriganeshsilktraders/invoice/invoiceGenerator/service/EmailService.java
+++ src/main/java/com/sriganeshsilktraders/invoice/invoiceGenerator/service/EmailService.java
@@ -9,23 +9,23 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-    private final JavaMailSender javaMailSender;
-
-    public  void sendVerificationOtpEmail(String userEmail,String otp,String subject,String text) throws MessagingException {
-        try{
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,"utf-8");
-
-            mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText(text);
-            mimeMessageHelper.setTo(userEmail);
-            javaMailSender.send(mimeMessage);
-        }catch (MailException e){
-            System.out.println("erroooooorrrr ---"+e);
-            throw  new MailSendException("fail to send email");
-        }
-    }
-}
+//@Service
+//@RequiredArgsConstructor
+//public class EmailService {
+//    private final JavaMailSender javaMailSender;
+//
+//    public  void sendVerificationOtpEmail(String userEmail,String otp,String subject,String text) throws MessagingException {
+//        try{
+//            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,"utf-8");
+//
+//            mimeMessageHelper.setSubject(subject);
+//            mimeMessageHelper.setText(text);
+//            mimeMessageHelper.setTo(userEmail);
+//            javaMailSender.send(mimeMessage);
+//        }catch (MailException e){
+//            System.out.println("erroooooorrrr ---"+e);
+//            throw  new MailSendException("fail to send email");
+//        }
+//    }
+//}
